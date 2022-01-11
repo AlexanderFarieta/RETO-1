71862726fb7075ccde9cb99f0612bad2b32590e8
AlexanderFarieta <96701928+AlexanderFarieta@users.noreply.github.com>
2022-01-11 11:07:10 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+import {currencies} from '../data/data.js';
+
+let moneda = 
+
+currencies.forEach(element => {
+    
+});
+
+function convertirMoneda(elem) {
+
+    let valor = document.getElementById("cantidad-dinero").value;
+    let valor = document.getElementById("moneda-origen").value;
+    let valor = document.getElementById("moneda-destino").value;
+
+}
+
+console.log (currencies)
